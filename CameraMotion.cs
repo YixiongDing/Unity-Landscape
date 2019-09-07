59bb626687a986d3933f973761dac6555b6d0143
Piccky <740823604@qq.com>
2019-09-07 17:30:54 +1000

    Add files via upload

---
+++ CameraMotion.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMotion : MonoBehaviour
+{
+    float speed = 100f;                 // Speed of camera
+    float camSens = .30f;               // Pitch and yaw sensitivity
+    float roll = 0f;                    // Absolute value of roll applied
+    Vector3 pos = new Vector3(0, 0, 0); // New position from K/B input
+
+    /** Previous heading is given an initial direction */
+    private Vector3 prevHead = new Vector3(Screen.width / 2, Screen.height / 2, 0);
+
+    public Transform model;
+    private const float default_distance = 500f;
+
+
+    void Start()
+    {
+        // 旋转归零
+        transform.rotation = Quaternion.identity;
+        // 初始位置是模型
+        Vector3 position = model.position;
+        position.z -= default_distance;
+        transform.position = position;
+    }
+
+
+    void Update()
+    {
+        /** Parse mouse headings */
+        prevHead = Input.mousePosition - prevHead;
+        prevHead = new Vector3(-prevHead.y * camSens, prevHead.x * camSens,
+                                roll);
+        prevHead = new Vector3(transform.eulerAngles.x + prevHead.x,
+                                transform.eulerAngles.y + prevHead.y, roll);
+        transform.eulerAngles = prevHead;
+        prevHead = Input.mousePosition;
+
+        /** Parse keyboard movements */
+        pos = getDirection();
+        pos = pos * speed;
+        pos = pos * Time.deltaTime;
+        transform.Translate(pos);
+
+    }
+
+    /** Keybindings for keyboard controls */
+    private Vector3 getDirection()
+    {
+        Vector3 vel = new Vector3();
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            vel += new Vector3(0, 0, 1);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            vel += new Vector3(0, 0, -1);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            vel += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            vel += new Vector3(1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            roll += 0.5f;
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            roll += -0.5f;
+        }
+        return vel;
+    }
+
+    /** Collision response */
+    void OnTriggerEnter(Collider col)
+    {
+        transform.Translate(-pos * 5);
+    }
+}
---
+++ SunOrbit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunOrbit : MonoBehaviour
+{
+    public Transform center;
+    public Vector3 axis = Vector3.up;
+    public Vector3 desiredPosition;
+    public float radius = 20.0f;
+    public float radiusSpeed = 80f;
+    public float rotationSpeed = 80.0f;
+
+    void Update()
+    {
+        transform.RotateAround(center.position, axis, rotationSpeed * Time.deltaTime);
+        desiredPosition = (transform.position - center.position).normalized * radius + center.position;
+        transform.position = Vector3.MoveTowards(transform.position, desiredPosition, radiusSpeed);
+    }
+}
